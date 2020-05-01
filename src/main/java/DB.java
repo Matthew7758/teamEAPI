7bf3b24ae518c395265caae25b9f066fc6a80080
matthew7758 <entei567@gmail.com>
2020-05-01 19:30:17 -0400

    Shortened DB.java

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -53,12 +53,6 @@ public class DB {
       Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
     } catch (ClassNotFoundException e) {
       System.out.println("Apache Derby Driver not found. Add the classpath to your module.");
-      System.out.println("For IntelliJ do the following:");
-      System.out.println("File | Project Structure, Modules, Dependency tab");
-      System.out.println("Add by clicking on the green plus icon on the right of the window");
-      System.out.println(
-          "Select JARs or directories. Go to the folder where the database JAR is located");
-      System.out.println("Click OK, now you can compile your program and run it.");
       e.printStackTrace();
       return null;
     }
