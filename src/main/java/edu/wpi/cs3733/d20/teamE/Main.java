185e82afcfbd07081d06a368dfb0364bc0cd0372
matthew7758 <entei567@gmail.com>
2020-05-06 01:06:26 -0400

    Jar can be built.

--- src/main/java/edu/wpi/cs3733/d20/teamE/Main.java
+++ src/main/java/edu/wpi/cs3733/d20/teamE/Main.java
@@ -6,22 +6,8 @@ import javafx.stage.Stage;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class Main extends Application {
+public class Main {
   public static void main(String[] args) {
-    Application.launch();
-  }
-
-  public void start(Stage primaryStage) throws Exception {
-    DB database = new DB("admin", "password");
-    Connection conn = database.connectDB("admin", "password");
-
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      try {
-        conn.close();
-      } catch (SQLException throwables) {
-        throwables.printStackTrace();
-      }
-    }));
-    onCallBeds.run(0, 0, 1280, 720, "edu/wpi/cs3733/d20/teamE/stylesheets/default.css", null, null);
+    App.launch(App.class,args);
   }
 }
