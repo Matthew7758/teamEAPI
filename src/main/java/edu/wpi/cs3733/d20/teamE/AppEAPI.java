3a236aa30be00e215afcda192325c3dba433b1e6
matthew7758 <entei567@gmail.com>
2020-05-07 12:12:50 -0400

    Fixed image links

---
+++ src/main/java/edu/wpi/cs3733/d20/teamE/AppEAPI.java
@@ -0,0 +1,23 @@
+package edu.wpi.cs3733.d20.teamE;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class AppEAPI extends Application {
+  public void start(Stage primaryStage) throws Exception {
+    DBEAPI database = new DBEAPI("admin", "password");
+    Connection conn = database.connectDB("admin", "password");
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      try {
+        conn.close();
+      } catch (SQLException throwables) {
+        throwables.printStackTrace();
+      }
+    }));
+    onCallBeds.run(0, 0, 1280, 720, "/edu/wpi/cs3733/d20/teamE/stylesheetsEAPI/defaultEAPI.css", null, null);
+  }
+}
